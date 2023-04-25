8a16fdc4fee9f012b70369e280a59397090e10fb
DhruvArora2025 <dhruvarora202520@gmail.com>
2023-04-25 18:39:01 -0400

    Finished problem 1.2 USACO practice.
    Learned how to use Vectors and scrapped using Arrays. Currently testing but on laptop the test data works. Using char variable for the data.

--- USACO_prep1/1.2/your_ride_is_here.cpp
+++ USACO_prep1/1.2/your_ride_is_here.cpp
@@ -7,20 +7,55 @@ LANG: C++
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include<numeric>
 
 using namespace std;
 
 ifstream fin("ride.in");
 ofstream fout("ride.out");
 
+// void printVector (vector<int> arr) 
+// {
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         cout << arr.at(i) << endl;
+//     }
+// }
+
 int main()
 {
     string a;
     string b;
     fin >> a >> b;
-    cout << a << " " << b << endl;
-    fout << a << " " << b << endl;
+    // fout << a << " " << b << endl;
+    
+    vector<int> arr;
+    for (char c : a) 
+    {
+       int i = c - 87 + 23;
+       arr.push_back(i);
+    }
 
-    return 0;
+    if (accumulate(arr.begin(), arr.end(), 1, multiplies<double>()) % 47 == 27) 
+    {
+        vector<int> arr2;
+        for (char c : b) 
+        {
+        int i = c - 87 + 23;
+        arr2.push_back(i);
+        }
+
+        if (accumulate(arr2.begin(), arr2.end(), 1, multiplies<double>()) % 47 == 27)
+        {
+            fout << "GO" << endl;
+        }
+        else 
+        {
+            fout << "STAY" << endl;
+        }
+    }
+    else {
+        fout << "STAY" << endl;
+    }
 }
-
