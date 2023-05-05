baa9cbef60e3dabc38eab83b59cf77063b62aa61
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-05 00:28:06 -0700

    Created map and solving using iteration and a while loop
    Learning how to move through a c++ file. Learning C++ syntax. Know how to solve the code.

--- USACO_prep1/1.2/greedy.cpp
+++ USACO_prep1/1.2/greedy.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <fstream>
+#include <map>
+#include <string>
 
 using namespace std;
 
@@ -11,7 +14,40 @@ int main()
     int np;
     fin >> np;
 
+    map<string, int> m;
     
+    for (int i = 0; i < np; i++) {
 
+        string name;
+        fin >> name;
+
+        m[name] = 0;
+    }
+
+    while (fin.peek() != EOF) {
+
+        string giver;
+        fin >> giver;
+
+        int gave, people;
+        fin >> gave;
+        fin >> people;
+        
+        int amount = gave / people; // amount per person
+
+        m[giver] -= amount; // removing the amount gifted
+        m[giver] += gave % people; // giving remainder money
+
+        for (int j = 0; j < people; j++) {
+            string person;
+            fin >> person;
+            m[person] += amount; // adding gift
+        }
+
+    }
+
+    for (auto const &pair : m) {
+        cout << pair.first << " " << pair.second << endl;
+    }
 
 }
