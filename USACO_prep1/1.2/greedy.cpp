16c0257649b2a733b728ccf2584c2ad132793ba5
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-08 18:57:57 -0700

    Found Solution
    I was missing an if (0)
    
    The calculation were always off because 0 is undefined causing an error. Everything else was correct. Correct answer in this commit.

--- USACO_prep1/1.2/greedy.cpp
+++ USACO_prep1/1.2/greedy.cpp
@@ -11,54 +11,38 @@ LANG: C++
 
 using namespace std;
 
-ifstream fin("gift1.in");
-ofstream fout("gift1.out");
-
 int main() 
 {
+    ifstream fin("gift1.in");
+	ofstream fout("gift1.out");
 
-    int np;
-    fin >> np;
+	int NP = 0, amount = 0, friends = 0;
+    fin >> NP;
 
-    map<string, int> m;
-    string order[np];
-    
-    for (int i = 0; i < np; i++) {
+	string names[NP];
+	map<string, int> sums;
 
-        string name;
-        fin >> name;
+	for(int i = 0; i < NP; i++) { // adding the names to names array
+		fin >> names[i];
+		sums[names[i]] = 0;
+	}
 
-        m[name] = 0;
-        order[i] = name;
-    }
+    string name;
+	for(int i = 0; i < NP; i++) {
+		fin >> name;
+		fin >> amount >> friends;
+		if (friends == 0) continue;
+		
+		int share = amount / friends;
+		sums[name] -= share * friends;
+		for(int x = 0; x < friends; x++) {
+			fin >> name;
+			sums[name] += share;
+		}
+	}
 
-    while (fin.peek() != EOF) {
-
-        string giver;
-        fin >> giver;
-
-        int gave, people;
-        fin >> gave;
-        fin >> people;
-        
-        int amount = gave / people; // amount per person
-        
-
-        m[giver] -= gave; // removing the amount gifted
-        m[giver] += gave % people; // giving remainder money
-
-        for (int j = 0; j < people; j++) {
-            string person;
-            fin >> person;
-            m[person] += amount; // adding gift
-        }
-
-    }
-
-
-    for (string name : order) {
-
-        fout << name << " " << m[name] << endl;
-
-    }
+	for (int i = 0; i < NP; i++) { // outputing the totals
+		fout << names[i] << " " << sums[names[i]] << endl;
+	}
+	
 }
