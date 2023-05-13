c43b31e3a52dcb3a750cc1492833c731eb49de99
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-13 00:47:27 -0700

    Finished Mathematical Algorithm
    Codeforces problem BunLover
    - Finished coding the algorithm.
    - Works but need to debug why it doesn't completely execute the code.

--- CodeForces/800/BunLover.cpp
+++ CodeForces/800/BunLover.cpp
@@ -6,5 +6,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello";
+
+    int t, n; 
+    cin >> t;
+
+    int total;
+    for (int x = 0; x < n; x++) {
+        cin >> n;
+
+        total = 10;
+        total += n*4;
+
+        for (int i = 5; i <= n; i++) {
+            total += (i-1) + (i-2);
+        }
+
+        cout << total;
+
+    }
 }
