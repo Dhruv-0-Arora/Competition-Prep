4ef9d18fe9d4ae71ad47cd64946ae9bf70c59693
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-20 12:36:46 -0700

    Submitted problem Bun Lover
    Solution implemented took too long.
    Implemented new algorithm to make it run faster

--- CodeForces/900/BunLover.cpp
+++ CodeForces/900/BunLover.cpp
@@ -1,6 +1,5 @@
 // https://codeforces.com/problemset/problem/1822/C
 
-#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -12,25 +11,20 @@ int main() {
     cin >> t;
 
     // total is the total number of buns
-    long long total;
+    long long totals[t];
     
     // loop through each test case
     for (int x = 0; x < t; x++) {
         // n is the number of buns in each set
         cin >> n;
 
-        // total > 10 because n > 4
-        total = 10;
-        total += n*4;
+        // calculating the total amount of chocolate
+        totals[x] = (1LL * n + 1)*(1LL * n + 1) + 1;
 
-        // calculate the chocolate for each bun
-        for (int i = 5; i <= n; i++) {
-            // adding two lines for each level of the bun
-            total += (i-1) + (i-2);
-        }
 
-        // print the total chocolate on the buns
-        cout << total;
+    }
 
+    for (long long total : totals) {
+        cout << total << endl;
     }
 }
