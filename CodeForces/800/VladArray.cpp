db17b029099311b76b42380b4ed4bf9c294946e2
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-25 00:11:14 -0700

    Trying to understand solution for problem (simple solution)

--- CodeForces/800/VladArray.cpp
+++ CodeForces/800/VladArray.cpp
@@ -5,74 +5,39 @@ int main() {
 
     int t;
     cin >> t;
+    int altT = t;
 
     string ans[t];
 
-    while(t--) {
+    while (t--)
+    {
+
         int n;
         cin >> n;
 
-        long long a[n];
-        int odd = 0;
-        int even = 0;
-        bool beautiful = true;
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            if (a[i] < 0) 
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        sort(arr, arr + n);
+
+        if (arr[0] % 2 == 1)
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 == 1)
             {
-                beautiful = false; 
+                cout << "YES" << endl;
                 break;
             }
-            if (a[i] % 2 == 0) even++;
-            else odd++;
         }
 
-        if (!beautiful) {
-            ans[t] = "NO";
-            continue;
-        }
-
-        if (odd == 0 || even == 0) {
-            ans[t] = "YES";
-            continue;
-        }
-
-        sort(a, a + n);
-
-        // for even numbers
-        bool beautifulEven = true;
-        for (int i = 0; i < n; i++) {
-            if (a[i] % 2 == 0) continue;
-            for (int j = 0; j < i; j++) 
-            {
-                if (a[j] % 2 == 0) break;
-                else {beautifulEven = false; break;}
-            }
-        }
-        
-        // for odd numbers
-        bool beautifulOdd = true;
-        for (int i = 0; i < n; i++) 
-        {
-            if (a[i] % 2 != 0) continue;
-            for (int j = 0; j < i; j++) 
-            {
-                if (a[j] % 2 != 0) break;
-                else {beautifulOdd = false; break;}
-            }
-        }
-
-        if (beautifulEven || beautifulOdd) ans[t] = "YES";
-        else ans[t] = "NO";
-
-
+        cout << "YES" << endl;
     }
 
-    cout << sizeof(ans) << endl;
-
-    for (int i = 0; i < sizeof(ans); i++) {
-        cout << ans[i] << endl;
-    }
     return 0;
 
 }
