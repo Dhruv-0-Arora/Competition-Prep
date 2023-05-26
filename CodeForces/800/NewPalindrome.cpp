3e1fdedb33a83ca25161a8974110914749e809cd
Dhruv Arora <dhruvarora202520@gmail.com>
2023-05-25 23:36:27 -0700

    Solving New Palindrome problem
    Using own algorithm to find if there are multiple letters.

---
+++ CodeForces/800/NewPalindrome.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        
+        vector<char> set1;
+        bool found = false;
+        for (int i = 0; i < s.length(); i++)
+        {
+            // how to check if a char is in a vector
+            if (find(set1.begin(), set1.end(), s[i]) != set1.end())
+            {
+                if (set1.size() >= 3 && s.size() % 2 != 0)
+                {
+                    cout << "YES" << endl;
+                    found = true;
+                    break;
+                }
+                else if (set1.size() >= 2 && s.size() % 2 == 0)
+                {
+                    cout << "YES" << endl;
+                    found = true;
+                    break;
+                }
+            }
+            else
+            {
+                set1.push_back(s[i]);
+            }
+            
+        }
+        if (!found)
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+
+}
+
+/*
+A palindrome is a string that reads the same from left to right as from right to left. For example, abacaba, aaaa, abba, racecar are palindromes.
+
+You are given a string s
+ consisting of lowercase Latin letters. The string s
+ is a palindrome.
+
+You have to check whether it is possible to rearrange the letters in it to get another palindrome (not equal to the given string s
+).
+
+Input
+The first line contains a single integer t
+ (1≤t≤1000
+) — the number of test cases.
+
+The only line of each test case contains a string s
+ (2≤|s|≤50
+) consisting of lowercase Latin letters. This string is a palindrome.
+
+Output
+For each test case, print YES if it is possible to rearrange the letters in the given string to get another palindrome. Otherwise, print NO.
+
+You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).
+*/
