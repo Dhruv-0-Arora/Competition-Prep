1b72009e7bd0d2258235f8198f9587e10748032e
DhruvArora2025 <dhruvarora202520@gmail.com>
2023-06-07 14:04:13 -0700

    Finished Problem IanVisitsMary (learning constructive algorithms
    Additionally, fixing intelisense mode to gcc -> homebrew. No more highlighting for the iostream

---
+++ CodeForces/800/IanVisitsMary.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a, b; cin >> a >> b; // the coordinates of the lattice point where Mary lives.
+
+    cout << 2 << endl;
+    cout << a - 1 << " " << 1 << endl;
+    cout << a << " " << b << endl;
+
+}
+
+int main()
+{
+    int t; cin >> t;
+    while (t--) solve();
+}
